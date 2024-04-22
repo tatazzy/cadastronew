214ebeea23fdd579d0b4794244a9d911b649959b
tatazzy <thatylla.vrr@gmail.com>
2024-04-22 14:44:39 -0300

    criar funcionario

--- src/components/ListaFuncionario.jsx
+++ src/components/ListaFuncionario.jsx
@@ -1,125 +1,144 @@
-import React, { useState, useEffect } from "react";
-import "./ListaFuncionario.css";
-
-const initialData = [
-  { id: 1, name: "João", age: 30, email: "joao@example.com" },
-  { id: 2, name: "Maria", age: 25, email: "maria@example.com" },
-  { id: 3, name: "Pedro", age: 28, email: "pedro@example.com" },
-  { id: 4, name: "Ana", age: 35, email: "ana@example.com" },
-  { id: 5, name: "Lucas", age: 32, email: "lucas@example.com" },
-  { id: 6, name: "Carla", age: 27, email: "carla@example.com" },
-  { id: 7, name: "Gabriel", age: 29, email: "gabriel@example.com" },
-  { id: 8, name: "Mariana", age: 31, email: "mariana@example.com" },
-  { id: 9, name: "Fernando", age: 33, email: "fernando@example.com" },
-  { id: 10, name: "Juliana", age: 26, email: "juliana@example.com" },
-  { id: 11, name: "Rafael", age: 29, email: "rafael@example.com" },
-  { id: 12, name: "Amanda", age: 24, email: "amanda@example.com" },
-  { id: 13, name: "Gustavo", age: 30, email: "gustavo@example.com" },
-  { id: 14, name: "Laura", age: 28, email: "laura@example.com" },
-  { id: 15, name: "Rodrigo", age: 34, email: "rodrigo@example.com" },
-];
+import React, { useState, useEffect } from "react"
+import styles from "./ListaFuncionario.module.css"
+import { listaFuncionario } from "../api/lista-funcionario"
 
 export function ListaFuncionario() {
-  const [people, setPeople] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sorted, setSorted] = useState(false);
-  const [expandedId, setExpandedId] = useState(null);
-  const [filteredLetter, setFilteredLetter] = useState("");
+  const [initialData, setInitialData] = useState([])
+  const [people, setPeople] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [sorted, setSorted] = useState(false)
+  const [expandedId, setExpandedId] = useState(null)
+  const [filteredLetter, setFilteredLetter] = useState("")
+
+  console.log("initialData", initialData)
+  console.log("people", people)
+
+  async function handleListEmployees() {
+    const response = await listaFuncionario()
+    console.log("response", response)
+    if (response?.dados) {
+      setInitialData(response.dados)
+    }
+  }
 
   useEffect(() => {
-    setPeople(initialData.slice().sort((a, b) => a.name.localeCompare(b.name)));
-  }, []);
+    handleListEmployees()
+  }, [])
+
+  useEffect(() => {
+    if (initialData?.length) {
+      setPeople(
+        initialData.slice().sort((a, b) => a.nome.localeCompare(b.nome))
+      )
+    }
+  }, [initialData])
 
   const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+    setSearchTerm(event.target.value)
 
     const filteredPeople = initialData.filter((person) =>
-      person.name.toLowerCase().includes(event.target.value.toLowerCase())
-    );
-    setPeople(filteredPeople);
-  };
+      person.nome.toLowerCase().includes(event.target.value.toLowerCase())
+    )
+    setPeople(filteredPeople)
+  }
 
   const handleSort = () => {
     setPeople((prevPeople) =>
-      prevPeople.slice().sort((a, b) => a.name.localeCompare(b.name))
-    );
-    setSorted(true);
-  };
+      prevPeople.slice().sort((a, b) => a.nome.localeCompare(b.nome))
+    )
+    setSorted(true)
+  }
 
   const handleItemClick = (id) => {
-    setExpandedId((prevId) => (prevId === id ? null : id));
-  };
+    setExpandedId((prevId) => (prevId === id ? null : id))
+  }
 
   const handleLetterClick = (letter) => {
     if (filteredLetter === letter) {
-      setFilteredLetter("");
+      setFilteredLetter("")
       setPeople(
-        initialData.slice().sort((a, b) => a.name.localeCompare(b.name))
-      );
+        initialData.slice().sort((a, b) => a.nome.localeCompare(b.nome))
+      )
     } else {
-      setFilteredLetter(letter);
+      setFilteredLetter(letter)
       const filteredPeople = initialData.filter((person) =>
-        person.name.toLowerCase().startsWith(letter.toLowerCase())
-      );
-      setPeople(filteredPeople);
+        person.nome.toLowerCase().startsWith(letter.toLowerCase())
+      )
+      setPeople(filteredPeople)
     }
-  };
+  }
 
   const handleBack = () => {
-    window.location.reload();
-  };
+    window.location.reload()
+  }
 
   const handleHome = () => {
-    window.location.reload();
-  };
+    window.location.reload()
+  }
 
   return (
     <div className="App">
-      <div className="top-buttons">
-        <button onClick={handleBack}>Voltar</button>
-        <button onClick={handleHome}>Página Inicial</button>
+      <div className={styles["top-buttons"]}>
+        <button className={styles["button"]} onClick={handleBack}>
+          Voltar
+        </button>
+        <button className={styles["button"]} onClick={handleHome}>
+          Página Inicial
+        </button>
       </div>
-      <div className="container">
-        <h1>Lista de Pessoas</h1>
+      <div className={styles["container"]}>
+        <h1 className={styles["title"]}>Lista de Pessoas</h1>
         <input
+          className={styles.input}
           type="text"
           placeholder="Pesquisar"
           value={searchTerm}
           onChange={handleSearch}
         />
-        <div className="button-container">
+        <div className={styles["button-container"]}>
           {Array.from({ length: 26 }, (_, index) =>
             String.fromCharCode(65 + index)
           ).map((letter) => (
             <button
+              className={`${styles["button"]} ${
+                filteredLetter === letter ? "active" : ""
+              }`}
               key={letter}
               onClick={() => handleLetterClick(letter)}
-              className={filteredLetter === letter ? "active" : ""}
             >
               {letter}
             </button>
           ))}
         </div>
-        <ul className="people-list">
+        <ul className={`${styles["people-list"]} ${styles.ul}`}>
           {sorted
             ? people.map((person) => (
-                <li key={person.id} onClick={() => handleItemClick(person.id)}>
-                  {person.name}
+                <li
+                  className={`${styles.li}`}
+                  key={person.id}
+                  onClick={() => handleItemClick(person.id)}
+                >
+                  <p>{person.nome}</p>
                   {expandedId === person.id && (
-                    <div className="details">
-                      <p>Idade: {person.age}</p>
+                    <div className={styles["details"]}>
+                      {console.log("person", person)}
                       <p>Email: {person.email}</p>
+                      <p>Nome: {person.nome}</p>
                     </div>
                   )}
                 </li>
               ))
             : people.map((person) => (
-                <li key={person.id} onClick={() => handleItemClick(person.id)}>
-                  {person.name}
+                <li
+                  className={`${styles.li}`}
+                  key={person.id}
+                  onClick={() => handleItemClick(person.id)}
+                >
+                  <p>{person.nome}</p>
                   {expandedId === person.id && (
-                    <div className="details">
-                      <p>Idade: {person.age}</p>
+                    <div className={styles["details"]}>
                       <p>Email: {person.email}</p>
+                      <p>Nome: {person.nome}</p>
                     </div>
                   )}
                 </li>
@@ -127,5 +146,5 @@ export function ListaFuncionario() {
         </ul>
       </div>
     </div>
-  );
+  )
 }
